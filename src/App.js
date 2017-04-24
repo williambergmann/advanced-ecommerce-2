7771bd2fa43a2b861a79c1f53a3c637b4adf808d
Jon Woo <jw56578@gmail.com>
2017-04-23 19:10:35 -0500

    add files

---
+++ src/App.js
@@ -0,0 +1,214 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import state from "./state";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        	<div className="wrap">
+	
+			<div className="header">
+	
+			<div className="top-header">
+				<div className="top-header-left">
+					<ul>
+						<li className="active"><a href="#">Home</a></li>
+						<li><a href="#">Specials</a></li>
+						<li><a href="#">Delivery</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</div>
+				<div className="top-header-right">
+					<ul>
+						<li><a href="#">CURRENCY:</a></li>
+						<li>
+							<select>
+								<option>Dollar</option>
+								<option>Euro</option>
+								<option>Pound</option>
+							</select>
+						</li>
+					</ul>
+				</div>
+				<div className="clear"> </div>
+			</div>
+			<div className="clear"> </div>
+			<div className="sub-header">
+				<div className="logo">
+					<a href="index.html"><img src="images/logo.png" title="logo" /></a>
+				</div>
+				<div className="sub-header-right">
+					<ul>
+						<li><a href="#">log in</a></li>
+						<li><a href="#">Your account</a></li>
+						<li><a href="#">CART: (EMPTY) <img src="images/cart.png" title="cart" /></a></li>
+					</ul>
+					<input type="text" /><input type="submit"  value="search" />
+				</div>
+				<div className="clear"> </div>
+			</div>
+			<div className="clear"> </div>
+			<div className="top-nav">
+				<ul>
+					<li><a href="carlights.html">car lights</a></li>
+					<li><a href="carwheels.html">Car wheels</a></li>
+					<li><a href="carbumpers.html">car bumpers</a></li>
+					<li><a href="caradsystem.html">car audiosystem</a></li>
+					<li><a href="truckbumpers.html">Truck bumpers</a></li>
+					<li><a href="contact.html">Feedback</a></li>
+					<div className="clear"> </div>
+				</ul>
+			</div>
+		
+			</div>
+			
+		
+					<div className="image-slider">
+				
+					    <ul className="rslides" id="slider1">
+					      <li><img src="images/slider1.jpg" alt="" /></li>
+					      <li><img src="images/slider3.jpg" alt="" /></li>
+					      <li><img src="images/slider1.jpg" alt="" /></li>
+					    </ul>
+					
+					</div>
+				
+				<div className="content">
+					<div className="products-box">
+					<div className="products">
+						<h5><span>FEATURED</span> PRODUCTS</h5>
+						<div className="section group">
+							<div className="grid_1_of_5 images_1_of_5">
+								 <img src="images/g3.png" />
+								 <h3>Lorem Ipsum is simply </h3>
+								 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
+								 <h4>$512.00</h4>
+							     <div className="button"><span><a href="singlepage.html">Read More</a></span></div>
+						   </div>
+							<div className="grid_1_of_5 images_1_of_5">
+								 <img src="images/g1.jpg" />
+								 <h3>Lorem Ipsum is simply </h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
+								<h4>$300.00</h4>
+							     <div className="button"><span><a href="singlepage.html">Read More</a></span></div>
+						    </div>
+							<div className="grid_1_of_5 images_1_of_5">
+								<img src="images/g2.jpg" />
+								 <h3>Lorem Ipsum is simply </h3>
+								 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
+								 <h4>$120.00</h4>
+							     <div className="button"><span><a href="singlepage.html">Read More</a></span></div>
+							</div>
+							<div className="grid_1_of_5 images_1_of_5">
+								 <img src="images/g3.png" />
+								 <h3>Lorem Ipsum is simply </h3>
+								 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
+								 <h4>$500.00</h4>
+							     <div className="button"><span><a href="singlepage.html">Read More</a></span></div>
+							</div>
+							<div className="grid_1_of_5 images_1_of_5">
+								 <img src="images/g4.jpg" />
+								 <h3>Lorem Ipsum is simply</h3>
+								 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
+								 <h4>$120.00</h4>
+							     <div className="button"><span><a href="singlepage.html">Read More</a></span></div>
+							</div>
+						</div>
+					</div>
+					<div className="products products-secondbox">
+						<h5><span>Our</span> Specials</h5>
+						<div className="section group">
+							<div className="grid_1_of_5 images_1_of_5">
+								 <img src="images/g1.jpg" />
+								 <h3>Lorem Ipsum is simply </h3>
+								 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
+								 <h4><span>$600.00</span>$512.00</h4>
+							     <div className="button"><span><a href="singlepage.html">Read More</a></span></div>
+						   </div>
+							<div className="grid_1_of_5 images_1_of_5">
+								 <img src="images/g6.jpg" />
+								 <h3>Lorem Ipsum is simply </h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
+								 <h4><span>$400.00</span>$352.00</h4>
+							     <div className="button"><span><a href="singlepage.html">Read More</a></span></div>
+						    </div>
+							<div className="grid_1_of_5 images_1_of_5">
+								<img src="images/g7.png" />
+								 <h3>Lorem Ipsum is simply </h3>
+								 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
+								  <h4><span>$300.00</span>$202.00</h4>
+							     <div className="button"><span><a href="singlepage.html">Read More</a></span></div>
+							</div>
+							<div className="grid_1_of_5 images_1_of_5">
+								 <img src="images/g8.png" />
+								 <h3>Lorem Ipsum is simply </h3>
+								 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
+								 <h4><span>$400.00</span>$322.00</h4>
+							     <div className="button"><span><a href="singlepage.html">Read More</a></span></div>
+							</div>
+							<div className="grid_1_of_5 images_1_of_5">
+								 <img src="images/g1.jpg" />
+								 <h3>Lorem Ipsum is simply</h3>
+								 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
+								 <h4><span>$700.00</span>$602.00</h4>
+							     <div className="button"><span><a href="singlepage.html">Read More</a></span></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="clear"> </div>
+			<div className="footer">
+				<div className="section group">
+				<div className="col_1_of_4 span_1_of_4">
+					<h3>INFORMATION</h3>
+					<ul>
+						<li><a href="#">About us</a></li>
+						<li><a href="#">Sitemap</a></li>
+						<li><a href="#">Contact</a></li>
+						<li><a href="#">Terms and conditions</a></li>
+						<li><a href="#">Legal Notice</a></li>
+					</ul>
+				</div>
+				<div className="col_1_of_4 span_1_of_4">
+					<h3>OUR OFFERS</h3>
+					<ul>
+						<li><a href="#">New products</a></li>
+						<li><a href="#">top sellers</a></li>
+						<li><a href="#">Specials</a></li>
+						<li><a href="#">Products</a></li>
+						<li><a href="#">Comments</a></li>
+					</ul>
+				</div>
+				<div className="col_1_of_4 span_1_of_4">
+					<h3>YOUR ACCOUNT</h3>
+					<ul>
+						<li><a href="#">Your Account</a></li>
+						<li><a href="#">Personal info</a></li>
+						<li><a href="#">Prices</a></li>
+						<li><a href="#">Address</a></li>
+						<li><a href="#">Locations</a></li>
+					</ul>
+				</div>
+				<div className="col_1_of_4 span_1_of_4 footer-lastgrid">
+					<h3>Get in touch</h3>
+					<ul>
+						<li><a href="#"><img src="images/facebook.png" title="facebook" /></a></li>
+						<li><a href="#"><img src="images/twitter.png" title="Twiiter" /></a></li>
+						<li><a href="#"><img src="images/rss.png" title="Rss" /></a></li>
+						<li><a href="#"><img src="images/gpluse.png" title="Google+" /></a></li>
+					</ul>
+					<p>Design by <a href="#">W3layouts</a></p>
+				</div>
+			</div>
+			</div>
+		</div>
+	
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/state.js
@@ -0,0 +1,272 @@
+export default {
+  products:[{
+  "id": 1,
+  "name": "Namfix",
+  "description": "Extraction of Nasal Turbinate, Open Approach",
+  "price": "$23.85",
+  "imgUrl": "/images/t1.jpg",
+  "category": "tires",
+  "rating": 3,
+  "reviews": 5
+}, {
+  "id": 2,
+  "name": "Opela",
+  "description": "Insert Spacer in R Finger Phalanx Jt, Perc Endo",
+  "price": "$0.63",
+   "imgUrl": "/images/t2.jpg",
+  "category": "tires",
+  "rating": 3,
+  "reviews": 4
+}, {
+  "id": 3,
+  "name": "Tampflex",
+  "description": "Excision of Pudendal Nerve, Percutaneous Approach, Diagn",
+  "price": "$79.50",
+   "imgUrl": "/images/t3.jpg",
+  "category": "tires",
+  "rating": 1,
+  "reviews": 1
+}, {
+  "id": 4,
+  "name": "Aerified",
+  "description": "Repair Prepuce, Percutaneous Endoscopic Approach",
+  "price": "$19.76",
+   "imgUrl": "/images/t4.jpg",
+  "category": "tires",
+  "rating": 3,
+  "reviews": 4
+}, {
+  "id": 5,
+  "name": "Y-Solowarm",
+  "description": "Repair Right Pleura, Percutaneous Approach",
+  "price": "$10.39",
+   "imgUrl": "/images/t5.jpg",
+  "category": "tires",
+  "rating": 1,
+  "reviews": 4
+}, {
+  "id": 6,
+  "name": "Trippledex",
+  "description": "Bypass Left Kidney Pelvis to Ileum, Perc Endo Approach",
+  "price": "$76.24",
+  "imgUrl": "/images/b1.jpg",
+  "category": "bumpers",
+  "rating": 1,
+  "reviews": 4
+}, {
+  "id": 7,
+  "name": "Flowdesk",
+  "description": "Excision of Right Inguinal Lymphatic, Open Approach",
+  "price": "$29.81",
+   "imgUrl": "/images/b2.jpg",
+  "category": "bumpers",
+  "rating": 3,
+  "reviews": 3
+}, {
+  "id": 8,
+  "name": "Temp",
+  "description": "Dilate L Int Iliac Art, Bifurc, w 3 Drug-elut, Perc",
+  "price": "$2.07",
+   "imgUrl": "/images/b3.jpg",
+  "category": "bumpers",
+  "rating": 3,
+  "reviews": 5
+}, {
+  "id": 9,
+  "name": "Ventosanzap",
+  "description": "Drainage of Right Lower Lung Lobe, Via Opening",
+  "price": "$57.59",
+   "imgUrl": "/images/b4.jpg",
+  "category": "bumpers",
+  "rating": 2,
+  "reviews": 4
+}, {
+  "id": 10,
+  "name": "Aerified",
+  "description": "Removal of Infusion Device from Mediastinum, Open Approach",
+  "price": "$89.68",
+   "imgUrl": "/images/b5.jpg",
+  "category": "bumpers",
+  "rating": 2,
+  "reviews": 1
+}, {
+  "id": 11,
+  "name": "Stringtough",
+  "description": "Revision of Radioactive Element in Abd Wall, Open Approach",
+  "price": "$28.78",
+  "imgUrl": "http://dummyimage.com/199x107.png/ff4444/ffffff",
+  "category": "Gembucket",
+  "rating": 2,
+  "reviews": 3
+}, {
+  "id": 12,
+  "name": "Zaam-Dox",
+  "description": "Restriction of R Lg Intest with Intralum Dev, Perc Approach",
+  "price": "$46.67",
+  "imgUrl": "http://dummyimage.com/173x189.bmp/cc0000/ffffff",
+  "category": "Zathin",
+  "rating": 3,
+  "reviews": 4
+}, {
+  "id": 13,
+  "name": "Asoka",
+  "description": "Excision of Upper Tooth, Open Approach, Single",
+  "price": "$35.28",
+  "imgUrl": "http://dummyimage.com/127x194.jpg/ff4444/ffffff",
+  "category": "Trippledex",
+  "rating": 1,
+  "reviews": 5
+}, {
+  "id": 14,
+  "name": "Hatity",
+  "description": "Insert Infusion Dev in Prostate/Seminal Ves, Perc",
+  "price": "$51.42",
+  "imgUrl": "http://dummyimage.com/119x250.bmp/cc0000/ffffff",
+  "category": "Flowdesk",
+  "rating": 1,
+  "reviews": 2
+}, {
+  "id": 15,
+  "name": "Bamity",
+  "description": "Excision of Left Testis, Percutaneous Endoscopic Approach",
+  "price": "$83.34",
+  "imgUrl": "http://dummyimage.com/131x193.png/dddddd/000000",
+  "category": "Konklux",
+  "rating": 4,
+  "reviews": 2
+}, {
+  "id": 16,
+  "name": "Temp",
+  "description": "LDR Brachytherapy of Trachea using Palladium 103",
+  "price": "$29.93",
+  "imgUrl": "http://dummyimage.com/239x137.jpg/ff4444/ffffff",
+  "category": "Tampflex",
+  "rating": 5,
+  "reviews": 3
+}, {
+  "id": 17,
+  "name": "Aerified",
+  "description": "Revision of Spacer in L Finger Phalanx Jt, Extern Approach",
+  "price": "$77.88",
+  "imgUrl": "http://dummyimage.com/120x219.bmp/cc0000/ffffff",
+  "category": "Job",
+  "rating": 2,
+  "reviews": 2
+}, {
+  "id": 18,
+  "name": "Overhold",
+  "description": "Drainage of Left Upper Leg Tendon, Perc Endo Approach, Diagn",
+  "price": "$16.28",
+  "imgUrl": "http://dummyimage.com/249x226.png/cc0000/ffffff",
+  "category": "Prodder",
+  "rating": 4,
+  "reviews": 3
+}, {
+  "id": 19,
+  "name": "Andalax",
+  "description": "Release Left Subclavian Vein, Percutaneous Approach",
+  "price": "$65.18",
+  "imgUrl": "http://dummyimage.com/203x222.bmp/dddddd/000000",
+  "category": "Matsoft",
+  "rating": 5,
+  "reviews": 2
+}, {
+  "id": 20,
+  "name": "Latlux",
+  "description": "Excision of Left Kidney, Percutaneous Endoscopic Approach",
+  "price": "$34.48",
+  "imgUrl": "http://dummyimage.com/154x116.bmp/ff4444/ffffff",
+  "category": "Lotstring",
+  "rating": 2,
+  "reviews": 2
+}, {
+  "id": 21,
+  "name": "Flowdesk",
+  "description": "Revision of Drain Dev in L Metacarpophal Jt, Open Approach",
+  "price": "$21.31",
+  "imgUrl": "http://dummyimage.com/145x184.jpg/5fa2dd/ffffff",
+  "category": "Fix San",
+  "rating": 3,
+  "reviews": 2
+}, {
+  "id": 22,
+  "name": "Tresom",
+  "description": "Dilate of L Low Lobe Bronc with Intralum Dev, Perc Approach",
+  "price": "$4.03",
+  "imgUrl": "http://dummyimage.com/112x138.jpg/5fa2dd/ffffff",
+  "category": "Konklux",
+  "rating": 4,
+  "reviews": 2
+}, {
+  "id": 23,
+  "name": "Voyatouch",
+  "description": "Removal of Synth Sub from R Pleural Cav, Perc Endo Approach",
+  "price": "$90.33",
+  "imgUrl": "http://dummyimage.com/140x245.png/dddddd/000000",
+  "category": "Alphazap",
+  "rating": 2,
+  "reviews": 5
+}, {
+  "id": 24,
+  "name": "Solarbreeze",
+  "description": "Bypass L Subclav Art to Low Arm Vein w Autol Art, Open",
+  "price": "$10.92",
+  "imgUrl": "http://dummyimage.com/156x179.bmp/5fa2dd/ffffff",
+  "category": "Andalax",
+  "rating": 5,
+  "reviews": 1
+}, {
+  "id": 25,
+  "name": "Konklux",
+  "description": "Restriction of Left Main Bronchus, Via Opening",
+  "price": "$72.31",
+  "imgUrl": "http://dummyimage.com/143x167.png/5fa2dd/ffffff",
+  "category": "Flowdesk",
+  "rating": 2,
+  "reviews": 4
+}, {
+  "id": 26,
+  "name": "Latlux",
+  "description": "Insertion of Infusion Dev into Fem Perineum, Open Approach",
+  "price": "$33.06",
+  "imgUrl": "http://dummyimage.com/126x143.png/ff4444/ffffff",
+  "category": "Kanlam",
+  "rating": 2,
+  "reviews": 5
+}, {
+  "id": 27,
+  "name": "Stim",
+  "description": "Transfer Olfact Nrv to Trochlear Nrv, Perc Endo Approach",
+  "price": "$69.86",
+  "imgUrl": "http://dummyimage.com/220x249.png/5fa2dd/ffffff",
+  "category": "Quo Lux",
+  "rating": 2,
+  "reviews": 5
+}, {
+  "id": 28,
+  "name": "Greenlam",
+  "description": "Bypass L Innom Vein to Up Vein w Autol Sub, Perc Endo",
+  "price": "$63.77",
+  "imgUrl": "http://dummyimage.com/199x135.jpg/dddddd/000000",
+  "category": "Sub-Ex",
+  "rating": 5,
+  "reviews": 3
+}, {
+  "id": 29,
+  "name": "Fintone",
+  "description": "Beam Radiation of Rectum using Photons 1 - 10 MeV",
+  "price": "$26.56",
+  "imgUrl": "http://dummyimage.com/179x215.jpg/ff4444/ffffff",
+  "category": "Fixflex",
+  "rating": 5,
+  "reviews": 5
+}, {
+  "id": 30,
+  "name": "Stim",
+  "description": "Ultrasonography of Right Lower Extremity Veins, Guidance",
+  "price": "$47.57",
+  "imgUrl": "http://dummyimage.com/215x112.bmp/dddddd/000000",
+  "category": "Span",
+  "rating": 2,
+  "reviews": 1
+}]};
